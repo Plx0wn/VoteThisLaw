bddffcd290da379f1a112b416d858e77c8bda7c0
Plx0wn <lucas.rouss@gmail.com>
2017-04-22 18:11:12 +0200

    Update VoteTheLaw.java

--- src/fr.plx0wn/VoteTheLaw.java
+++ src/fr.plx0wn/VoteTheLaw.java
@@ -1,13 +1,22 @@
 package fr.plx0wn;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.plx0wn.AssemblyButton.Button;
+import fr.plx0wn.AssemblyButton.ButtonEvents;
 import net.md_5.bungee.api.ChatColor;
 
 public class VoteTheLaw extends JavaPlugin {
@@ -15,158 +24,225 @@ public class VoteTheLaw extends JavaPlugin {
 	public static boolean vote = false;
 	public static ArrayList<Player> vote_yes = new ArrayList<>();
 	public static ArrayList<Player> vote_no = new ArrayList<>();
+	public static Plugin instance;
+	public static File msgfile;
+	public static FileConfiguration msgconf;
 
-	public String cs(String msg) {
+	public static String cs(String msg) {
 		return ChatColor.translateAlternateColorCodes('&', msg);
 	}
 
 	public void onEnable() {
-		getConfig().options().copyDefaults();
+		instance = this;
+		PluginManager pm = Bukkit.getPluginManager();
+		pm.registerEvents(new ButtonEvents(), instance);
 		saveDefaultConfig();
+		createFiles();
+	}
+	
+	private void createFiles() {
+
+		msgfile = new File(getDataFolder(), "messages.yml");
+		if (!msgfile.exists()) {
+			msgfile.getParentFile().mkdirs();
+			saveResource("messages.yml", false);
+		}
+		try {
+			msgconf = new YamlConfiguration();
+			msgconf.load(msgfile);
+		} catch (InvalidConfigurationException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (sender instanceof Player) {
 
-			Player player = ((Player) sender).getPlayer();
+		// PARTIE JOUEUR & CONSOLE
 
-			if (label.equalsIgnoreCase("vote")) {
-				if (args.length == 0) {
-					// LISTE DES COMMANDES
-					player.sendMessage("");
-					player.sendMessage(cs("Liste des commmandes:"));
-					if(player.hasPermission("vote.start")){
-						player.sendMessage(cs("&a- /vote start"));
-					}
-					if(player.hasPermission("vote.stop")){
-						player.sendMessage(cs("&a- /vote stop"));
-					}
-					player.sendMessage(cs("&a- /vote pour"));
-					player.sendMessage(cs("&a- /vote contre"));
-					player.sendMessage("");
+		if (label.equalsIgnoreCase("vote")) {
+			if (args.length == 0) {
+				// LISTE DES COMMANDES
+				sender.sendMessage("");
+				sender.sendMessage(cs("&aListe des commmandes:"));
+				if (sender.hasPermission("vote.start")) {
+					sender.sendMessage(cs("&a- /vote start"));
 				}
-				if (args.length == 1) {
-					
-					
-					if (args[0].equalsIgnoreCase("start")) {
-						if (player.hasPermission("vote.start")) {
-							if (vote == false) {
-								// REMISE A ZERO
-								vote_yes.clear();
-								vote_no.clear();
+				if (sender.hasPermission("vote.stop")) {
+					sender.sendMessage(cs("&a- /vote stop"));
+				}
+				sender.sendMessage(cs("&a- /vote pour"));
+				sender.sendMessage(cs("&a- /vote contre"));
+				sender.sendMessage("");
+			}
+			if (args.length == 1) {
 
-								// CONTENTS
-								vote = !vote;
-								Bukkit.getServer().broadcastMessage("");
-								Bukkit.getServer().broadcastMessage(cs(getConfig().getString("messages.start")));
-								Bukkit.getServer().broadcastMessage("");
-							} else {
-								player.sendMessage(cs(getConfig().getString("messages.already-start")));
-							}
+				if (args[0].equalsIgnoreCase("start")) {
+					if (sender.hasPermission("vote.start")) {
+						if (vote == false) {
+							// REMISE A ZERO
+							vote_yes.clear();
+							vote_no.clear();
+
+							// CONTENTS
+							vote = !vote;
+
+							// NO WORDS
+
+							Bukkit.getServer().broadcastMessage("");
+							Bukkit.getServer().broadcastMessage(cs(msgconf.getString("messages.start")));
+							Bukkit.getServer().broadcastMessage("");
+
 						} else {
-							player.sendMessage(cs(getConfig().getString("messages.no-perms")));
+							sender.sendMessage(cs(msgconf.getString("messages.already-start")));
 						}
+					} else {
+						sender.sendMessage(cs(msgconf.getString("messages.no-perms")));
 					}
-					
-					
-					if (args[0].equalsIgnoreCase("stop")) {
-						if (player.hasPermission("vote.stop")) {
-							if (vote == true) {
-								vote = !vote;
-								;
-								int yes = vote_yes.size();
-								int no = vote_no.size();
-								Bukkit.getServer().broadcastMessage("");
-								Bukkit.getServer().broadcastMessage(cs(getConfig().getString("messages.stop")));
-								Bukkit.getServer().broadcastMessage("");
-								Bukkit.getServer().broadcastMessage(cs(getConfig().getString("messages.results")
-										.replace("[yes]", "" + yes).replace("[no]", "" + no)));
-								if (yes > no) {
+
+				}
+
+				if (args[0].equalsIgnoreCase("stop")) {
+					if (sender.hasPermission("vote.stop")) {
+						if (vote == true) {
+							vote = !vote;
+							;
+							int yes = vote_yes.size();
+							int no = vote_no.size();
+							Bukkit.getServer().broadcastMessage("");
+							Bukkit.getServer().broadcastMessage(cs(msgconf.getString("messages.stop")));
+							Bukkit.getServer().broadcastMessage("");
+							Bukkit.getServer().broadcastMessage(cs(msgconf.getString("messages.results")
+									.replace("[yes]", "" + yes).replace("[no]", "" + no)));
+							if (yes > no) {
+								Bukkit.getServer().broadcastMessage(cs(msgconf.getString("messages.law-accepted")));
+							} else {
+								if (yes == no) {
 									Bukkit.getServer()
-											.broadcastMessage(cs(getConfig().getString("messages.law-accepted")));
+											.broadcastMessage(cs(msgconf.getString("messages.law-equality")));
 								} else {
-									if (yes == no) {
-										Bukkit.getServer()
-										.broadcastMessage(cs(getConfig().getString("messages.law-equality")));
-									} else {
-										Bukkit.getServer()
-												.broadcastMessage(cs(getConfig().getString("messages.law-refused")));
-									}
-
+									Bukkit.getServer()
+											.broadcastMessage(cs(msgconf.getString("messages.law-refused")));
 								}
-								Bukkit.getServer().broadcastMessage("");
 
-							} else {
-								player.sendMessage(cs(getConfig().getString("messages.no-vote")));
 							}
+							Bukkit.getServer().broadcastMessage("");
+
 						} else {
-							player.sendMessage(cs(getConfig().getString("messages.no-perms")));
+							sender.sendMessage(cs(msgconf.getString("messages.no-vote")));
 						}
+					} else {
+						sender.sendMessage(cs(msgconf.getString("messages.no-perms")));
 					}
-					
-					
+				}
+
+				if (args[0].equalsIgnoreCase("reload")) {
+					if (sender.hasPermission("vote.reload")) {
+						try {
+							reloadConfig();
+							sender.sendMessage(ChatColor.GREEN + "Configs rechargés !");
+						} catch (Exception e) {
+							e.printStackTrace();
+							sender.sendMessage(ChatColor.RED + "Les configs n'ont pu être rechargés.");
+						}
+					} else {
+						sender.sendMessage(cs(msgconf.getString("messages.no-perms")));
+					}
+				}
+
+				// PARTIE JOUEUR
+
+				if (sender instanceof Player) {
+					Player player = ((Player) sender).getPlayer();
 					if (args[0].equalsIgnoreCase("pour")) {
 						if (player.hasPermission("vote.vote")) {
 							if (vote == true) {
 								if (vote_yes.contains(player)) {
-									player.sendMessage(cs(getConfig().getString("messages.already-vote")));
+									player.sendMessage(cs(msgconf.getString("messages.already-vote")));
 								} else {
 									vote_yes.add(player);
-									player.sendMessage(cs(getConfig().getString("messages.vote-yes")));
+									player.sendMessage(cs(msgconf.getString("messages.vote-yes")));
 									if (vote_no.contains(player)) {
 										vote_no.remove(player);
 									}
 								}
 							} else {
-								player.sendMessage(cs(getConfig().getString("messages.no-vote")));
+								player.sendMessage(cs(msgconf.getString("messages.no-vote")));
 							}
 						} else {
-							player.sendMessage(cs(getConfig().getString("messages.no-perms")));
+							player.sendMessage(cs(msgconf.getString("messages.no-perms")));
 						}
 					}
-					
-					
+
 					if (args[0].equalsIgnoreCase("contre")) {
 						if (player.hasPermission("vote.vote")) {
 							if (vote == true) {
 								if (vote_no.contains(player)) {
-									player.sendMessage(cs(getConfig().getString("messages.already-vote")));
+									player.sendMessage(cs(msgconf.getString("messages.already-vote")));
 								} else {
 									vote_no.add(player);
-									player.sendMessage(cs(getConfig().getString("messages.vote-no")));
+									player.sendMessage(cs(msgconf.getString("messages.vote-no")));
 									if (vote_yes.contains(player)) {
 										vote_yes.remove(player);
 									}
 								}
 							} else {
-								player.sendMessage(cs(getConfig().getString("messages.no-vote")));
+								player.sendMessage(cs(msgconf.getString("messages.no-vote")));
 							}
 						} else {
-							player.sendMessage(cs(getConfig().getString("messages.no-perms")));
+							player.sendMessage(cs(msgconf.getString("messages.no-perms")));
 						}
 					}
 					
+					// SETUP (mise en place des buttons personnalisé.
 					
-					if (args[0].equalsIgnoreCase("reload")) {
-						if (player.hasPermission("vote.reload")) {
-							try {
-								reloadConfig();
-								player.sendMessage(ChatColor.GREEN + "Configs rechargés !");
-							} catch (Exception e) {
-								e.printStackTrace();
-								player.sendMessage(ChatColor.RED + "Les configs n'ont pu être rechargés.");
-							}
-						} else {
-							player.sendMessage(cs(getConfig().getString("messages.no-perms")));
-						}
+					if(args[0].equalsIgnoreCase("setup")){
+						
 					}
-					
-
 				}
+				
+				
 			}
-		}
-		return false;
-	}
+			
+			
+			if (args.length >= 2) {
+				
+				
+				if(sender instanceof Player){
+					// SETUP (mise en place des buttons personnalisé.
+					Player player = ((Player)sender).getPlayer();
+					
+					if(args.length == 2){
+						if(args[0].equalsIgnoreCase("setup")){
+							if(args[1].equalsIgnoreCase("start")){
+								//Button.spawnVoteStartButton(player);
+								player.sendMessage(ChatColor.RED + "Dans la prochaine version.");
+							}
+							if(args[1].equalsIgnoreCase("stop")){
+								//Button.spawnVoteStopButton(player);
+								player.sendMessage(ChatColor.RED + "Dans la prochaine version.");
+							}
+						}
+					}
+				}
+				
+				// VOTE START WITH SENTENCE
+				
+				if(args[0].equalsIgnoreCase("start")){
+					String s = "";
+					for (int i = 1; i < args.length; i++) {
+						String arg = args[i] + " ";
+						s = s + arg;
+					}
+					
+					Bukkit.getServer().broadcastMessage("");
+					Bukkit.getServer().broadcastMessage(cs(msgconf.getString("messages.start")));
+					Bukkit.getServer().broadcastMessage(cs("&a" + s));
+					Bukkit.getServer().broadcastMessage("");
+				}
+				
+			}
+		}return false;
+}
 
 }
